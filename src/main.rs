392cb2ac42435f1a6f1463f8db3eeb8974f2a56c
Ezekiel McCrary <zekemc2020@gmail.com>
2024-09-04 23:47:29 -0400

    Added preliminary Token struct and tokenize function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
 fn main() {
-    println!("Hello, world!");
+
+}
+
+
+
+
+
+/* Consider moving below section to its own file later */
+
+// #[derive(Debug)]
+enum TokenType {
+    Word,
+    Letter,
+    SpecialCharacter,
+}
+
+// #[derive(Debug)]
+struct Token<'a> {
+    tipe: TokenType,
+    text: &'a str,
+}
+
+
+fn tokenize<'a>(input: &'a str) -> Vec<Token<'a>> {
+    todo!()
 }
