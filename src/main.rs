91d22106ecc99770b9946c0e6d0482335de63b42
Ezekiel McCrary <zekemc2020@gmail.com>
2024-09-10 23:15:31 -0400

    renamed care to chair

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,29 @@
 mod parse;
 
-use crate::parse::tokenize;
+use crate::parse::parse;
 
-const DICT: &'static str = "./cmu_dict.json";
+use serde_json::{self, Map, Value};
+use std::fs;
+
+const CMU: &'static str = "/Users/mccraryezekiel/Documents/code/phoneticenglish/src/resources/cmu_dict.json";
+const CONVERSION: &'static str = "/Users/mccraryezekiel/Documents/code/phoneticenglish/src/resources/conversions.json";
 
 
 fn main() {
-    let stir = "Hi. T!h!e ostrick-catcher wasn't dead!!";
+    let parse_map = get_map_from_file(&CMU);
+    let conversion_map = get_map_from_file(&CONVERSION);
+    let text = "Hello. Hi. T!h!e ostrick-catcher wasn't dead!!";
 
-    let token_vec = tokenize(stir);
+    let new_text = parse(text, parse_map, conversion_map);
 
-    println!("{:?}", token_vec);
+    println!("{new_text}");
 }
 
 
+fn get_map_from_file(filepath: &str) -> Map<String, Value> {
+    // TODO: since we know Value will always be the Array(Vec<Value>) variant,
+    //       change the Map<String, Value> to Map<String, Vec<String>> somehow
+    let db = fs::read_to_string(filepath).expect("Could not read file");
+    
+    serde_json::from_str(&db).expect("Failed to read json")
+}
--- src/parse.rs
+++ src/parse.rs
@@ -1,3 +1,34 @@
+use serde_json::{Value, Map};
+
+
+pub fn parse<'a>(text: &'a str, parse_map: Map<String, Value>, conversion_map: Map<String, Value>) -> String {
+    // parse to token vec
+    let token_vec = tokenize(text);
+    
+    let mut output = String::with_capacity(token_vec.len());
+
+    for token in token_vec.iter() {
+    // TODO: remove Space enum variant
+        match token {
+            Token::Space => output.push(' '),
+            Token::SpecialCharacter(c) => output.push(*c),
+            Token::Word(s) => {
+                if let Some(val) = parse_map.get(*s) {
+                    output.push_str(&translate_word(val.get(0).unwrap().as_str().unwrap(), &conversion_map))
+                }
+                else {
+                    output.push_str(&format!("[{s}]"));
+                }
+            }
+        }
+    }
+
+    output
+}
+
+
+
+
 #[derive(Debug)]
 pub enum Token<'a> {
     Space,
@@ -5,25 +36,24 @@ pub enum Token<'a> {
     Word(&'a str),
 }
 
-
-pub fn tokenize<'a>(input: &'a str) -> Vec<Token<'a>> {
+fn tokenize<'a>(input: &'a str) -> Vec<Token<'a>> {
     let separated: std::str::Split<'a, char> = input.split(' ');
     let mut output: Vec<Token<'a>> = Vec::new();
 
     for word in separated {
         let mut start_index: usize = 0;
 
-        for (i, care) in word.chars().enumerate() {
+        for (i, chair) in word.chars().enumerate() {
         // TODO: could this all be a match statement?
-            // if `care` is a letter or apostrophe, it's part of a word
-            if care.is_ascii_alphabetic() || care == '\'' { continue; }
+            // if `chair` is a letter or apostrophe, it's part of a word
+            if chair.is_ascii_alphabetic() || chair == '\'' { continue; }
 
             // otherwise, the word is over, so push it into the token vector
             if start_index != i {
                 output.push(Token::Word(&word[start_index..i]));
             }
             // and then push the non-letter non-apostrophe character
-            output.push(Token::SpecialCharacter(care));
+            output.push(Token::SpecialCharacter(chair));
 
             // and reset `start_index`
             start_index = i + 1;
@@ -41,3 +71,32 @@ pub fn tokenize<'a>(input: &'a str) -> Vec<Token<'a>> {
 
     output
 }
+
+
+fn translate_word<'a>(word_str: &'a str, conversion_map: &Map<String, Value>) -> String {
+    let mut string_builder = String::with_capacity(word_str.len() /* >> 1 ? */);
+
+    for phoneme in word_str.split(' ') {
+        // if phoneme length is 3 it has a number on the end we don't use (it is ok to slice the str because the map only has ascii)
+        let shortened = if phoneme.len() == 3 { &phoneme[..2] } else { phoneme };
+
+        if let Some(val) = conversion_map.get(shortened) {
+            string_builder.push_str(val.as_str().unwrap())
+        }
+        else { string_builder.push_str(shortened) }
+    }
+
+    string_builder
+}
+
+
+
+/*
+fn read_json<'a>(filepath: &'a str) -> Map<String, Value> {
+    // TODO: since we know Value will always be the Array(Vec<Value>) variant,
+    //       change the Map<String, Value> to Map<String, Vec<String>> somehow
+    let cmu_db = fs::read_to_string(filepath).expect("Could not read file");
+    
+    serde_json::from_str(&cmu_db).expect("Failed to read json")
+}
+    */
